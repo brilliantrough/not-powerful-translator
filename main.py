070dde0eadefe3c43c32c1a93222274f8117eccb
brilliantrough <pzyinnju@163.com>
2024-03-01 23:03:36 +0800

    version 2.0.1

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Author: brilliantrough pzyinnju@163.com
+Date: 2024-02-29 02:29:49
+LastEditors: brilliantrough pzyinnju@163.com
+LastEditTime: 2024-02-29 02:30:35
+FilePath: \not-powerful-translator\main.py
+Description: 
+
+Copyright (c) 2024 by {brilliantrough pzyinnju@163.com}, All Rights Reserved. 
+'''
+from mainwindow import main
+
+if __name__ == "__main__":
+    main()
---
+++ utils/sst/OCR/__init__.py
@@ -0,0 +1,2 @@
+from .switch_image import switch_image
+from .ocr_process import ocr_process
