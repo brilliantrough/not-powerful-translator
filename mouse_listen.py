59ae01641102180da43182bb5b3041d5e03461b1
brilliantrough <pzyinnju@163.com>
2025-01-07 16:21:49 +0800

    [update]: 更新鼠标划词逻辑

--- mouse_listen.py
+++ mouse_listen.py
@@ -9,63 +9,61 @@ Description:
 Copyright (c) 2023 by {brilliantrough pzyinnju@163.com}, All Rights Reserved. 
 """
 
-from pyperclip import copy, paste
-from pynput.keyboard import Key, Controller
-from pynput.mouse import Controller as MouseController
-from pynput import mouse
-import time
-from threading import Condition
 import os
+import sys
+import threading
+import time
+
+from pynput import mouse
+from pynput.keyboard import Controller, GlobalHotKeys, Key, KeyCode
+from pyperclip import copy, paste
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QCursor
+
 from utils.mycls import Log
+from utils.settings import (
+    GlobalConditionVariables,
+    MutableSharedSettings,
+    RuntimeSettings,
+    get_MOUSE_LISTEN_KEEP_GOING,
+    set_MOUSE_LISTEN_KEEP_GOING,
+)
 
-from PyQt5.QtCore import pyqtSignal, QThread
-
-# create a condition variable for the main thread to wait for the mouse release event
-cv = Condition()
-
-keyboard = Controller()  # create a keyboard controller
-mouse1 = MouseController()  # create a mouse controller
 TIME_TO_WAIT = 0.4  # the time to wait for the mouse release event
 t = time.time()  # the time when the mouse is pressed
-last_selected_text = ""  # the last selected text, initialized to empty
-original_text = ""  # the original text, initialized to empty
-translate_num = 1  # the number of the translation, initialized to 1
-pause_flag = False  # whether the translation is paused, initialized to False
 
 
-def on_click(x, y, button, pressed):
-    """the click event handler, with the press and release event
-
-    Args:
-        x (int): the position of the mouse on the x axis, do not use
-        y (int): the position of the mouse on the y axis, do not use
-        button (Button): which button is pressed
-        pressed (bool): whether the button is pressed or released
-    """
-    global t, pause_flag
-    if pause_flag:
-        return
-    if pressed and button == mouse.Button.left:
-        t = time.time()
-        # print('press on {0} at {1}'.format(x, y))
-    elif not pressed and button == mouse.Button.left:
-        # print('release on {0} at {1}'.format(x, y))
-        time_margin = time.time() - t
-        # print("the time margin is", time_margin)
-        if time_margin > TIME_TO_WAIT:
-            with cv:
-                cv.notify_all()
+def on_activate():
+    print("Global hotkey activated!")
 
 
 class MouseListener(QThread):
     selectText = pyqtSignal(str)
+    popButton = pyqtSignal()
+    triggerHotkey = pyqtSignal()
+    clickOutsideButton = pyqtSignal(int, int)  # New signal
+    clickAutoHide = pyqtSignal(int, int)
 
     def __init__(self):
         super(MouseListener, self).__init__()
-        self.listener = mouse.Listener(on_click=on_click)
+        self.listener = mouse.Listener(on_click=self.on_click)
+        self.keyboard_listener = GlobalHotKeys({"<alt>+q": self.triggerHotkey.emit})
         self.running: bool = True
         self.stop: bool = False
-        # self.log = Log("log.txt")
+        self.log = Log("log.txt")
+        self.triggerTranslate = False
+        self.read_text: list[str] = ["", ""]
+        self.last_copy_text: str = ""
+        self.last_selected_text = ""  # the last selected text, initialized to empty
+        self.selected_text = ""  # the selected text, initialized to empty
+        self.original_text = ""  # the original text, initialized to empty
+        self.translate_num = 1  # the number of the translation, initialized to 1
+        self.reserve_text: str = ""
+        self.keyboard = Controller()  # create a keyboard controller
+        if sys.platform == "win32":
+            self.getSelectedText = self.getSelectedTextWin32
+        else:
+            self.getSelectedText = self.getSelectedTextLinux
 
     def run(self):
         """the main thread, to translate the selected text
@@ -79,68 +77,237 @@ class MouseListener(QThread):
         * if the selected text is the same as the original text, the translation will be skipped
         * if the translation failed up to 3 times, the translation will be skipped
         """
-        global original_text, translate_num, last_selected_text
         self.listener.start()  # start the mouse listener
+        self.keyboard_listener.start()  # start the keyboard listener
+        self.last_selected_text = (
+            "" if sys.platform == "win32" else os.popen("xsel").read()
+        )
         while True:
             if not self.running:
                 time.sleep(1)
                 continue
-            with cv:
-                cv.wait()
+            with GlobalConditionVariables.CV_MOUSE_LISTEN:
+                GlobalConditionVariables.CV_MOUSE_LISTEN.wait()
             if self.stop:
                 break
-            original_text = paste()
-            keyboard.press(Key.ctrl)
-            keyboard.press(Key.insert)
-            keyboard.release(Key.insert)
-            keyboard.release(Key.ctrl)  # copy the selected text
-            time.sleep(0.2)  # wait for the text to be copied
-            selected_text = paste()
-            # restore the original text
-            copy(original_text)
 
+            self.translateHotkeyCondition()
+            self.getSelectedText()
+            if not self.translateButtonCondition():
+                continue
+            self.translate()
+
+    def translate(self):
+        self.selectText.emit(self.selected_text)
+        self.triggerTranslate = False
+
+    def translateHotkeyCondition(self):
+        if RuntimeSettings.USE_HOTKEY and not RuntimeSettings.USE_BUTTON:
+            print("use hotkey")
+            set_MOUSE_LISTEN_KEEP_GOING(True)
+            with GlobalConditionVariables.CV_BUTTON_KEY_LISTEN:
+                GlobalConditionVariables.CV_BUTTON_KEY_LISTEN.wait()
+
+    def translateButtonCondition(self):
+        if not self.triggerTranslate:
+            return False
+        if (
+            RuntimeSettings.USE_BUTTON
+            and not MutableSharedSettings.POP_BUTTON
+            # and not get_MOUSE_LISTEN_KEEP_GOING()
+        ):
+            set_MOUSE_LISTEN_KEEP_GOING(True)
+            MutableSharedSettings.POP_BUTTON = True
+            self.popButton.emit()
+            with GlobalConditionVariables.CV_BUTTON_KEY_LISTEN:
+                GlobalConditionVariables.CV_BUTTON_KEY_LISTEN.wait()
+        if not get_MOUSE_LISTEN_KEEP_GOING():
+            set_MOUSE_LISTEN_KEEP_GOING(True)
+            return False
+        return True
+
+    def getSelectedTextLinux(self):
+        time.sleep(0.1)
+        print("getSelectedTextLinux")
+        self.selected_text = os.popen("xsel").read()
+        self.read_text[1] = self.selected_text
+        if (
+            self.selected_text != self.last_selected_text
+            and self.selected_text != ""
+            and self.selected_text != self.read_text[0]
+            and self.selected_text != self.reserve_text
+        ):
+            self.log.write("xsel: " + self.selected_text)
+            self.triggerTranslate = True
+            self.last_selected_text = self.selected_text
+        else:
+            self.original_text = paste()
+            self.log.write("orignal_text:\n" + self.original_text)
+
+            # Reset all possible held keys before simulating copy
+            self._reset_all_keys()
+
+            self.keyboard.press(Key.ctrl)
+            self.keyboard.press(Key.insert)
+            time.sleep(0.1)
+            self.keyboard.release(Key.insert)
+            self.keyboard.release(Key.ctrl)  # copy the selected text
+            time.sleep(0.15)  # wait for the text to be copied
+            self.selected_text = paste()
             if (
-                selected_text == last_selected_text
-                or selected_text == ""
-                or selected_text == original_text
+                self.selected_text != self.last_selected_text
+                and self.selected_text != ""
+                and self.selected_text != self.last_copy_text
+                and self.selected_text != self.original_text
             ):
-                keyboard.press(Key.ctrl)
-                keyboard.press("c")
-                keyboard.release("c")
-                keyboard.release(Key.ctrl)  # copy the selected text
-                time.sleep(0.2)  # wait for the text to be copied
-                selected_text = paste()
-                copy(original_text)
-                if (
-                    selected_text == last_selected_text
-                    or selected_text == ""
-                    or selected_text == original_text
-                ):
-                    continue
-                
-            # print("the selected text is", selected_text)
-            self.selectText.emit(selected_text)
-            last_selected_text = selected_text  # update the last selected text
-            translate_num += 1  # update the number of the translation
+                self.log.write("copied:" + self.selected_text)
+                self.triggerTranslate = True
+                self.last_copy_text = self.selected_text
+            # restore the original text
+            copy(self.original_text)
+        self.read_text[0] = self.read_text[1]
+        self.reserve_text = os.popen("xsel").read()
+        self.translate_num += 1  # update the number of the translation
+
+    def getSelectedTextWin32(self):
+        self.original_text = paste()
+
+        # Reset all possible held keys before simulating copy
+        self._reset_all_keys()
+
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press(Key.insert)
+        time.sleep(0.1)
+        self.keyboard.release(Key.insert)
+        self.keyboard.release(Key.ctrl)  # copy the selected text
+        time.sleep(0.3)  # wait for the text to be copied
+        self.selected_text = paste()
+        # restore the original text
+        copy(self.original_text)
+
+        if (
+            self.selected_text == self.last_selected_text
+            or self.selected_text == ""
+            or self.selected_text == self.original_text
+        ):
+            self.triggerTranslate = False
+            self.keyboard.press(Key.ctrl)
+            self.keyboard.press("c")
+            time.sleep(0.1)
+            self.keyboard.release("c")
+            self.keyboard.release(Key.ctrl)  # copy the selected text
+            time.sleep(0.3)  # wait for the text to be copied
+            self.selected_text = paste()
+            copy(self.original_text)
+            if (
+                self.selected_text == self.last_selected_text
+                or self.selected_text == ""
+                or self.selected_text == self.original_text
+            ):
+                self.triggerTranslate = False
+                return
+        self.triggerTranslate = True
+
+    def _reset_all_keys(self):
+        """Reset all possible held modifier keys."""
+        modifier_keys = [
+            Key.alt,
+            Key.alt_l,
+            Key.alt_r,
+            Key.ctrl,
+            Key.ctrl_l,
+            Key.ctrl_r,
+            Key.shift,
+            Key.shift_l,
+            Key.shift_r,
+            Key.cmd,
+            Key.cmd_l,
+            Key.cmd_r,
+        ]
+
+        for key in modifier_keys:
+            try:
+                self.keyboard.release(key)
+            except:
+                pass
 
     def pause(self):
         """pause the mouse listener"""
-        global pause_flag
-        pause_flag = True
+        RuntimeSettings.USE_SELECT_TEXT = False
+        RuntimeSettings.USE_MOUSE_LISTEN = False
 
     def resume(self):
         """resume the mouse listener"""
-        global pause_flag
-        pause_flag = False
+        RuntimeSettings.USE_SELECT_TEXT = True
+        RuntimeSettings.USE_MOUSE_LISTEN = True
 
     def quit(self):
-        """exit the mouse listener"""
-        if self.listener is not None:
-            mouse.Listener.stop(self.listener)
-            mouse.Listener.join(self.listener)
+        """Exit the mouse listener"""
+        print("mouseListener quit")
+        set_MOUSE_LISTEN_KEEP_GOING(False)
         self.stop = True
-        # del self.log
-        with cv:
-            cv.notify_all()
-        # time.sleep(0.1)
+
+        with GlobalConditionVariables.CV_MOUSE_LISTEN:
+            GlobalConditionVariables.CV_MOUSE_LISTEN.notify_all()
+        with GlobalConditionVariables.CV_BUTTON_KEY_LISTEN:
+            GlobalConditionVariables.CV_BUTTON_KEY_LISTEN.notify_all()
+
+        print("mouseListener quit 1")
+        # Stop the mouse listener
+        if self.listener is not None and self.listener.running:
+            self.listener.stop()
+            try:
+                # 创建一个线程来执行join，这样可以避免阻塞
+                join_thread = threading.Thread(target=self.listener.join)
+                join_thread.daemon = (
+                    True  # 设置为守护线程，这样主程序退出时它会自动结束
+                )
+                join_thread.start()
+                join_thread.join(timeout=0.5)  # 等待最多1秒
+            except:
+                pass  # 忽略任何可能的异常
+
+        print("mouseListener quit 2")
+
+        # Stop the keyboard listener
+        if self.keyboard_listener is not None and self.keyboard_listener.running:
+            self.keyboard_listener.stop()
+            try:
+                join_thread = threading.Thread(target=self.keyboard_listener.join)
+                join_thread.daemon = True
+                join_thread.start()
+                join_thread.join(timeout=0.5)
+            except:
+                pass
+
+        print("mouseListener quit 3")
+
         super().quit()
+
+    def on_click(self, x, y, button, pressed):
+        """the click event handler, with the press and release event
+
+        Args:
+            x (int): the position of the mouse on the x axis, do not use
+            y (int): the position of the mouse on the y axis, do not use
+            button (Button): which button is pressed
+            pressed (bool): whether the button is pressed or released
+        """
+        global t
+        if not RuntimeSettings.USE_MOUSE_LISTEN or not RuntimeSettings.USE_SELECT_TEXT:
+            return
+        if pressed and button == mouse.Button.left:
+            t = time.time()
+            if MutableSharedSettings.POP_BUTTON:
+                self.clickOutsideButton.emit(QCursor.pos().x(), QCursor.pos().y())
+            elif RuntimeSettings.USE_HOTKEY:
+                set_MOUSE_LISTEN_KEEP_GOING(False)
+                with GlobalConditionVariables.CV_BUTTON_KEY_LISTEN:
+                    GlobalConditionVariables.CV_BUTTON_KEY_LISTEN.notify_all()
+            if RuntimeSettings.AUTO_HIDE:
+                self.clickAutoHide.emit(QCursor.pos().x(), QCursor.pos().y())
+        elif not pressed and button == mouse.Button.left:
+            time_margin = time.time() - t
+            if time_margin > TIME_TO_WAIT:
+                with GlobalConditionVariables.CV_MOUSE_LISTEN:
+                    GlobalConditionVariables.CV_MOUSE_LISTEN.notify_all()
